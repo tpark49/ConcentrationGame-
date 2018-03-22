c4546cb939c31547f1f5dd4272f4d363164fbc28
Tony Park <typ5120@gmail.com>
2018-03-22 03:00:58 -0400

    added score label - will need to double check

--- MyConcentration/ViewController.swift
+++ MyConcentration/ViewController.swift
@@ -15,6 +15,82 @@ class ViewController: UIViewController {
     //flipcount variable
     var flipCount = 0
     
+    
+    //score variable
+    var scorePoints = 0
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    var scoreLib = [Int:Bool]()
+    func checkScore(){
+        for index in cardButton.indices{
+            let card = mygame.cards[index]
+            
+            if card.faceUp{
+                //if the card is not in the library
+                if scoreLib[card.identifier] == nil{
+                    if card.isMatched == true{
+                        scorePoints+=1
+                    }
+                    scoreLib[card.identifier] = true
+                }
+            
+                //if the card is already in the library
+                else if scoreLib[card.identifier] == true{
+                    if card.isMatched == false{
+                        scorePoints-=1
+                    }
+                }
+            
+                //skip the card if matched and seen before
+                if card.isMatched == true, scoreLib[card.identifier]==true{
+                    continue
+                }
+            
+            }
+        scoreLabel.text = "Score:" + String(scorePoints)
+        }}
+    
+    
+    func updateScore(at index: Int){
+        //find out if there is only one card faced up
+        var oneCardUp = false
+        var oneCardIndex = 0
+        for cardIndex in cardButton.indices{
+            if mygame.cards[cardIndex].faceUp == true, oneCardUp == false, index != cardIndex{
+                oneCardUp = true
+                oneCardIndex = cardIndex
+            }
+        }
+        var theOtherPair = 0
+        var theOtherPair_1 = 0
+        
+        //if only onecardup istrue
+        if oneCardUp == true{
+            if mygame.cards[index].isMatched == false{
+                if scoreLib[index] == nil{
+                    scoreLib[index] = true
+                }
+                for cardIndex in cardButton.indices{
+                    if mygame.cards[cardIndex].identifier == mygame.cards[oneCardIndex].identifier {
+                        theOtherPair = cardIndex
+                    }}
+                if scoreLib[theOtherPair] == true{
+                    scorePoints-=1
+                }
+            }
+        }
+        //if cards have matched
+        if mygame.cards[index].isMatched == true{
+            scorePoints += 2
+        }
+        scoreLabel.text = "Score:" + String(scorePoints)
+    }
+
+    
+        
+    
+    
     @IBAction func newGame(_ sender: UIButton) {
         //flip over all the cards
         var array = [Int]()
@@ -54,6 +130,9 @@ class ViewController: UIViewController {
         
         //update view
         updateView()
+        
+        //reset score
+        scorePoints = 0
     }
     
     
@@ -74,6 +153,9 @@ class ViewController: UIViewController {
             
             //update the view setting
             updateView()
+            
+            //update score
+            updateScore(at: cardNumber)
             
         }
         
